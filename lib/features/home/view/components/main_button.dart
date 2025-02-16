0e4cb56a35fa44140eb23d3e2f3e2b968cb703a6
shalarasgar <shalar.asgar@gmail.com>
2025-02-16 08:59:15 +0500

    home page ui

--- lib/features/home/view/components/main_button.dart
+++ lib/features/home/view/components/main_button.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'package:ruhun_sehbali/features/home/provider/navigation_provider.dart';
-
 
 class MainButton extends StatelessWidget {
   final String text;
@@ -15,23 +13,24 @@ class MainButton extends StatelessWidget {
       onTap: () {
         context.read<NavigationProvider>().setPage(nav);
         // context.read<MainBloc>().add(ResetInactivityTimerEvent());
-      
       },
       child: Container(
-        width: 60.w, // Set the desired width of the button
-        height: 30.h, // Set the desired height of the button
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 3),
+        constraints: const BoxConstraints(minHeight: 65, minWidth: 120),
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12.0),
-            color: const Color.fromARGB(255, 177, 146, 135) // Rounded corners
-            ),
-        child: Align(
-          alignment: Alignment.topCenter,
-          child: Text(
-            text, // Your button text
-            style: const TextStyle(
-              fontSize: 14,
-              color: Colors.white, // Text color for better contrast
-            ),
+          color: const Color(0xFFF0D9BC),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            letterSpacing: 1,
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            color: Colors.black,
+            decoration: TextDecoration.none,
           ),
         ),
       ),
